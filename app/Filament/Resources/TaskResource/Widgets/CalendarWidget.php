df38bdc595d34659859840f65e7f35b8631d8d78
Liam Li <liam.li@eatogether.com.tw>
2023-04-26 18:06:27 +0800

    [修改]FullCalendarWidget

--- app/Filament/Resources/TaskResource/Widgets/CalendarWidget.php
+++ app/Filament/Resources/TaskResource/Widgets/CalendarWidget.php
@@ -3,7 +3,13 @@
 namespace App\Filament\Resources\TaskResource\Widgets;
 
 use App\Models\Task;
-use Filament\Widgets\Widget;
+use App\Models\User;
+use App\Models\Restaurant;
+use Spatie\Permission\Models\Role;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
 use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
 
 // Your newly created widget should extends the Saade\FilamentFullCalendar\Widgets\FullCalendarWidget class of this package
@@ -14,8 +20,7 @@ class CalendarWidget extends FullCalendarWidget
 
     public function getViewData(): array
     {
-        $tasks = Task::all();
-        // dump($tasks);
+
         return [
             [
                 'id' => 1,
@@ -32,39 +37,51 @@ class CalendarWidget extends FullCalendarWidget
         ];
     }
 
-    /**
-     * FullCalendar will call this function whenever it needs new event data.
-     * This is triggered when the user clicks prev/next or switches views on the calendar.
-     */
-    public function fetchEvents(array $fetchInfo): array
+    public function createEvent(array $data): void
     {
-        // You can use $fetchInfo to filter events by date.
-        return [];
+
+        Task::create([
+            'category' => $data['category'],
+            'restaurant_id' => $data['restaurant_id'],
+            'user_id' => $data['user_id'],
+            'task_date' => $data['task_date'],
+        ]);
     }
 
-    /**
-     * Triggered when the user clicks an event.
-     */
-    public function onEventClick($event): void
+    protected static function getCreateEventFormSchema(): array
     {
-        parent::onEventClick($event);
+        $restaurants =  Restaurant::get();
 
-        // your code
-    }
+        $restaurantGroupByBrand = $restaurants->groupBy('brand')->keys();
 
-    /**
-     * Triggered when dragging stops and the event has moved to a different day/time.
-     */
-    public function onEventDrop($newEvent, $oldEvent, $relatedEvents): void
-    {
-        // your code
-    }
+        $restaurantGroupByShop = $restaurants->groupBy('shop')->keys();
 
-    /**
-     * Triggered when event's resize stops.
-     */
-    public function onEventResize($event, $oldEvent, $relatedEvents): void
-    {
-        // your code
+        $auditors = User::whereHas('roles', fn (Builder $query) => $query->where('name', 'auditor'))->get()->pluck('name', 'id');
+        return [
+            Select::make('category')
+                ->label('分類')
+                ->options([
+                    '食安及S5巡檢' => '食安及S5巡檢',
+                    '清潔檢查' => '清潔檢查',
+                    '餐點採樣' => '餐點採樣',
+                    '專案查核' => '專案查核',
+                ])
+                ->required(),
+            Select::make('brand')
+                ->label('品牌')
+                ->options($restaurantGroupByBrand)
+                ->required(),
+            Select::make('restaurant_id')
+                ->label('分店')
+                ->options($restaurantGroupByShop)
+                ->required(),
+            Select::make('user_id')
+                ->label('稽核員')
+                ->options($auditors)
+                ->required(),
+            DatePicker::make('task_date')
+                ->label('稽核日期')
+                ->required(),
+        ];
     }
 }
